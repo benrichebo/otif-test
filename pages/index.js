b069ebdd2ad3eb57dcbf2c378eccd3919b923f85
richard <richard@hubtel.com>
2022-08-22 19:01:15 -0300

    done with user authentication

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,66 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useUser } from "../hooks/useUser";
 
 function Home() {
+  const {
+    setUsername,
+    setPassword,
+    username,
+    password,
+    loading,
+    error,
+    login,
+  } = useUser();
+
+  //login method
+  //routes to users data pages after login
+  //states gets saved in local storage
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login();
+  };
+
   return (
-    <div>Home</div>
-  )
+    <div className="container d-flex justify-content-center align-items-center mx-auto vh-100">
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit} className="col-md-5 col-lg-4">
+        <div className="form-group mb-3">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            className="form-control"
+            name="username"
+            id="username"
+            aria-describedby="helpId"
+            placeholder=""
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="form-group mb-4">
+          <label htmlFor="password">Password</label>
+          <input
+            type="text"
+            className="form-control"
+            name="password"
+            id="password"
+            aria-describedby="helpId"
+            placeholder=""
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="form-group mb-4 w-100">
+          <button
+            type="submit"
+            className="btn btn-primary btn-block w-100"
+            disabled={!username || !password || loading}>
+            {loading ? "..." : "Submit"}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
